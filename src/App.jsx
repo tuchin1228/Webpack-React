21b088d74f3be109a6612596ad6974f297eae554
tuchin1228 <1104108110@nkust.edu.tw>
2021-05-09 17:05:19 +0800

    class component to hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,13 @@
-import React, { Component } from 'react'
-import picture from './assets/image/view.png'
-import axios from 'axios'
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  color: palevioletred;
-  span{
-    color:blue;
-  }
-`;
-
-export default class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      count: 100
-    }
-  }
-
-  componentDidMount() {
-    this.getData()
-  }
-  async getData() {
-    let res = await axios.get('https://run.mocky.io/v3/9d5f33ec-0d2f-4743-aac9-5aef32f7badf')
-    console.log(res);
-  }
+import React from 'react'
 
 
-  render() {
+const App = ()=>{
 
-    return (
-      <>
+  return(
+    <>
+      <h4 className="text-xl text-blue-400">tailwind</h4>
+    </>
+  )
 
-        <img src={picture} alt="" />
-        <Title>App Content<span>span</span></Title><br />
-        {this.state.count}<br />
-        <h4 className="text-xl text-blue-400">tailwind</h4>
-      </>
-    )
-
-  }
 }
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App.jsx'
 import "./tailwind/tailwind.css"
+import "./style/main.css"
 
 
 ReactDOM.render(
   <>
     <App />
-    Hello React
+    <h1>Hello React</h1>
   </>,
   document.getElementById("app")
 );
